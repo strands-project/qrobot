cd336b044c7ad9314f0b1ea069982ddfaf804017
Sergey Alexandrov <alexandrov88@gmail.com>
2016-06-13 23:17:43 +0200

    Add "stage" task to create release candidates

--- scripts/fabfile.py
+++ scripts/fabfile.py
@@ -1,134 +1,61 @@
 import os
 import os.path
-from fabric.api import cd, env, lcd, put, prompt, local, sudo, run
+from fabric.api import cd, env, lcd, put, prompt, local, sudo, run, local
 from fabric.contrib.files import exists
 
+HOST = '{STAGING_HOST}:{STAGING_SSH_PORT}'.format(**os.environ)
+ADMIN = os.environ['STAGING_ADMIN']
 
-project_name = 'qrobot'
+env.hosts = [HOST]
+env.user = ADMIN
 
-local_app_dir = './flask_project'
-local_config_dir = './config'
+REMOTE_HOME = '/home/qrobot'
+REMOTE_GIT = os.path.join(REMOTE_HOME, 'git')
+REMOTE_REPO = os.path.join(REMOTE_HOME, 'repo')
 
-remote_app_dir = '/home/www'
-remote_git_dir = '/home/git'
-remote_flask_dir = remote_app_dir + '/flask_project'
-remote_nginx_dir = '/etc/nginx/sites-enabled'
-remote_supervisor_dir = '/etc/supervisor/conf.d'
-
-env.hosts = ['localhost:2222']  # replace with IP address or hostname
-
-
-remote = {
-    'user': {
-        'admin': 'sergey',
-        'app': project_name
-    },
-    'dir': {
-        'base': os.path.join('/home', project_name),
-        'venv': os.path.join('/home', project_name, 'venv'),
-        'git': os.path.join('/home', project_name, 'git'),
-        'www': os.path.join('/home', project_name, 'www')
-    }
-}
-
-local = {
-    'dir': {
-        'base': os.environ['PROJECT_ROOT'],
-        'server': os.path.join(os.environ['PROJECT_ROOT'], 'server')
-    }
-}
-
-
-def install_requirements():
-    """
-    Install required packages.
-    """
-    env.user = remote['user']['admin']
-    sudo('apt-get update')
-    sudo('apt-get install -y python3')
-    sudo('apt-get install -y python3-dev')
-    sudo('apt-get install -y python-pip')
-    sudo('apt-get install -y python-virtualenv')
-    sudo('apt-get install -y nginx')
-    sudo('apt-get install -y supervisor')
-    sudo('apt-get install -y git')
-    sudo('apt-get install -y mongodb')
+LOCAL_CONFIG = './config'
 
 
 def create_user():
     """
-    Create a remote user for the project
+    Create a user for the app and setup passwordless login.
     """
-    env.user = remote['user']['admin']
-    sudo('adduser {} --disabled-password --gecos ""'.format(remote['user']['app']))
-    sudo('echo "{}:{}" | chpasswd'.format(remote['user']['app'], project_name))
+    sudo('adduser qrobot --disabled-password --gecos ""')
+    sudo('mkdir -p /home/qrobot/.ssh')
+    p = open(os.path.join(os.environ['HOME'], '.ssh', 'id_rsa.pub'), 'r').read()
+    sudo('echo "{}" >> /home/qrobot/.ssh/authorized_keys'.format(p))
+    sudo('chown qrobot:qrobot "/home/qrobot/.ssh" -R')
+    sudo('chmod 600 /home/qrobot/.ssh/authorized_keys')
 
 
-def setup_python():
+def setup_git():
     """
-    1. Create project directories
-    2. Create and activate a virtualenv
-    3. Copy Flask files to remote host
+    Setup git repositories (both local and remote).
     """
-    env.user = remote['user']['app']
-    #  if exists(remote_app_dir) is False:
-        #  sudo('mkdir ' + remote_app_dir)
-    #  if exists(remote_flask_dir) is False:
-        #  sudo('mkdir ' + remote_flask_dir)
-    with cd(remote['dir']['base']):
-        if not exists(remote['dir']['venv']):
-            run('virtualenv venv -p python3')
-            for reqfile in ['requirements.pip', 'requirements.production.pip']:
-                reqfile = os.path.join(local['dir']['server'], reqfile)
-                for package in open(reqfile, 'r').readlines():
-                    run('venv/bin/pip install {}'.format(package.strip()))
-    #  with cd(remote_flask_dir):
-        #  put('*', './', use_sudo=True)
-
-
-def configure_supervisor():
-    with lcd(local_config_dir):
-        with cd('/etc/supervisor/conf.d'):
-            put('supervisor', project_name + '.conf', use_sudo=True)
-    sudo('supervisorctl reread')
-    sudo('supervisorctl update')
-
-
-
-def configure_nginx():
-    """
-    1. Remove default nginx config file
-    2. Create new config file
-    3. Setup new symbolic link
-    4. Copy local config to remote config
-    5. Restart nginx
-    """
-    sudo('/etc/init.d/nginx start')
-    if exists('/etc/nginx/sites-enabled/default'):
-        sudo('rm /etc/nginx/sites-enabled/default')
-    config_en = os.path.join('/etc/nginx/sites-enabled', project_name)
-    config_av = os.path.join('/etc/nginx/sites-available', project_name)
-    if exists(config_en) is False:
-        sudo('touch ' + config_av)
-        sudo('ln -s ' + config_av + ' ' + config_en)
-    with lcd(local_config_dir):
-        with cd('/etc/nginx/sites-available'):
-            put('nginx', project_name, use_sudo=True)
-    sudo('/etc/init.d/nginx restart')
-
-
-def configure_git():
-    """
-    1. Setup bare Git repo
-    2. Create post-receive hook
-    """
-    env.user = remote['user']['app']
-    if exists(remote['dir']['git']) is False:
-        run('mkdir ' + remote['dir']['git'])
-        with cd(remote['dir']['git']):
+    env.user = 'qrobot'
+    local('git remote add staging ssh://qrobot@{}{}'.format(HOST, REMOTE_GIT))
+    if not exists(REMOTE_REPO):
+        run('mkdir ' + REMOTE_REPO)
+    if not exists(REMOTE_GIT):
+        run('mkdir ' + REMOTE_GIT)
+        with cd(REMOTE_GIT):
             run('git init --bare')
-            with lcd(local_config_dir):
+            local('git push staging master')
+            run('GIT_WORK_TREE="{}" git checkout -f'.format(REMOTE_REPO))
+            with lcd(LOCAL_CONFIG):
                 with cd('hooks'):
                     put('post-receive', 'post-receive', mode=0O775)
-    if not exists(remote['dir']['www']):
-        run('mkdir ' + remote['dir']['www'])
+
+def bootstrap():
+    """
+    Bootstrap the app from the repository.
+    """
+    env.user = ADMIN
+    with cd(REMOTE_REPO):
+        sudo('./scripts/bootstrap.sh production')
+
+
+def setup_staging():
+    create_user()
+    setup_git()
+    bootstrap()
--- scripts/tasks/__init__.py
+++ scripts/tasks/__init__.py
@@ -39,25 +39,41 @@ def publish():
     run('git push --tags')
 
 
+@task
+def stage():
+    os.chdir(os.environ['PROJECT_CLIENT'])
+    run('npm run build')
+    run('mkdir -p dist')
+    run('cp -r build/* dist')
+    run('git checkout -b build')
+    run('git add dist')
+    rev = get_next_revision_number()
+    run('git commit -m "Release candidate (revision {})"'.format(rev))
+    run('git push staging build:rc')
+    run('git checkout master')
+    run('git branch -D build')
+
+
 @task(post=[publish])
 def release():
     os.chdir(os.environ['PROJECT_CLIENT'])
-    print(run('npm run build'))
+    run('npm run build')
     run('mkdir -p dist')
     run('cp -r build/* dist')
-    run('git checkout -b pre-release')
+    run('git checkout -b build')
     run('git add dist')
     rev = get_next_revision_number()
     run('git commit -m "Release revision {}"'.format(rev))
     run('git tag r{}'.format(rev))
     run('git checkout master')
-    run('git branch -D pre-release')
+    run('git branch -D build')
 
 
 ns = Collection()
 ns.add_task(build)
 ns.add_task(install)
 ns.add_task(deploy)
+ns.add_task(stage)
 ns.add_task(release)
 ns.add_task(publish)
 ns.add_collection(tasks.db)
