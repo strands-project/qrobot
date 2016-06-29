f95223d4d108da2aed56f0d83b448b72bc5e26ba
Sergey Alexandrov <alexandrov88@gmail.com>
2016-06-29 18:56:35 +0200

    Shift code between TaskRunner and Task

--- client/src/components/task-runner/task-model.js
+++ client/src/components/task-runner/task-model.js
@@ -1,3 +1,4 @@
+var _ = require('lodash')
 var AmpersandModel = require('ampersand-model')
 
 module.exports = AmpersandModel.extend({
@@ -13,10 +14,32 @@ module.exports = AmpersandModel.extend({
     }
   },
 
-  initialize: function initialize (spec) {
-    spec = (spec || {})
-    this.fn = spec.fn || function (callback) {
-      callback(null)
-    }
+  initialize: function initialize (description, fn, minDuration) {
+    this.description = description || ''
+    this.minDuration = typeof minDuration === 'number' ? minDuration : 0
+    this.fn = _.bind(this.start, this, fn)
+  },
+
+  start: function start (fn, callback) {
+    this.started = (new Date()).getTime()
+    this.state = 'active'
+    this.callback = callback
+    fn(this.done.bind(this))
+  },
+
+  done: function (err, args) {
+    var duration = ((new Date()).getTime() - this.started)
+    var timeout = this.minDuration - duration
+    var task = this
+    _.delay(function () {
+      if (err === null) {
+        task.state = 'succeeded'
+        task.message = ''
+      } else {
+        task.state = 'failed'
+        task.message = err.message
+      }
+      task.callback(null) // here second argument (if given) replaces next callback!
+    }, timeout)
   }
 })
